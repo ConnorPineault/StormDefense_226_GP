5bc0b70e75492ac239a3ab6177835da07329c097
ConnorPineault <connor.pineault@gmail.com>
2024-02-25 15:45:08 -0500

    "weather" timer
    color = weather ( for now)
    
    weather changes evry 30 secs

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -8,8 +8,43 @@ public class LevelManager : MonoBehaviour
     public Transform[] path;
     public Transform startPoint;
 
-    private void Awake() {
+    private Camera mainCamera;
+
+
+
+    private Color[] weatherColors; //open arr 4 colors
+
+
+
+
+    private float weatherChangeInterval = 30f; //set 30s limit to timer
+    private float timer = 0f;
+
+    private void Awake()
+    {
         main = this;
+        mainCamera = Camera.main;
+
+
+
+  
+        weatherColors = new Color[] { Color.blue, Color.gray, Color.yellow, Color.cyan }; //color options
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= weatherChangeInterval)
+        {
+            ChangeWeather();
+            timer = 0f;
+        }
+    }
+
+    private void ChangeWeather()
+    {
+
+        Color randomColor = weatherColors[Random.Range(0, weatherColors.Length)];
+        mainCamera.backgroundColor = randomColor;
     }
 }
-
